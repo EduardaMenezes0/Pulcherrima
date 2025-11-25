7416c0518aa33a13a8dfc774f0a4eb2293fada93
EduardaMenezes0 <202313600036@ifba.edu.br>
2025-11-25 14:08:09 -0300

    atualização
    atualizações das páginas anteriores + fale conosco

---
+++ contato.php
@@ -0,0 +1,20 @@
+<?php
+include "DLL.php";
+extract($_POST);
+session_start();
+
+echo "
+    <html>
+      <head>
+        <title> Fale Conosco </title>
+        <link rel='stylesheet' href='style/style.css'>
+    </head>
+
+    <body class='login-body'>
+          <div class='login-wrapper'>
+          zap ; 98445420
+          </div>
+    </body>
+    
+          
+          
